fbdd971d7995262f00a7c2c75af986431d56b3a6
TBhutta <89844007+TBhutta@users.noreply.github.com>
2025-02-02 23:39:57 +0000

    Subscriptions page has been designed, though incomplete

--- youtube_clone/home/views.py
+++ youtube_clone/home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html", {})
+
+def subscriptions(request):
+    return render(request, "subscriptions-page.html", {})

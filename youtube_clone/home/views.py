638eeb102c9b544912b707a5589b8e7787cd4956
TBhutta <89844007+TBhutta@users.noreply.github.com>
2025-02-04 21:42:38 +0000

    Improved folder structure for better file navigation for templates and static files

--- youtube_clone/home/views.py
+++ youtube_clone/home/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "home.html", {})
+    return render(request, "home/home.html", {})
 
 def subscriptions(request):
-    return render(request, "subscriptions-page.html", {})
+    return render(request, "home/subscriptions-page.html", {})
 
 def history(request):
-    return render(request, "history-page.html", {})
+    return render(request, "home/history-page.html", {})
 
 def playlists(request):
-    return render(request, "playlists-page.html", {})
+    return render(request, "home/playlists-page.html", {})

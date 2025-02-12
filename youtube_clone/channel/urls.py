915df68b581561c388697eee9e7d6d5e8ba77381
TBhutta <89844007+TBhutta@users.noreply.github.com>
2025-02-12 22:13:44 +0000

    tried dynamic url with user's id in the url, but didn't work

--- youtube_clone/channel/urls.py
+++ youtube_clone/channel/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path("dashboard/<int:user_id>/", views.dashboard, name="dashboard"), # FIXME: figure out how to add account id in url
     path("dashboard/", views.dashboard, name="dashboard"),
 ]
--- youtube_clone/channel/views.py
+++ youtube_clone/channel/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def dashboard(request):
+def dashboard(request, user):
     return render(request, "channel/dashboard.html", {})

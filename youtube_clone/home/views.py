dc1d362c4740c84d4e3ef7685602a7c828bfd65f
TBhutta <89844007+TBhutta@users.noreply.github.com>
2025-02-21 21:27:25 +0000

    Managed to partially display saved video data on home page

--- youtube_clone/home/views.py
+++ youtube_clone/home/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
+from channel.models import Video
+from django.contrib.auth.models import User
 
 def home(request):
-    return render(request, "home/home.html", {})
+    recommended_videos = Video.objects.all()
+    ids = []
+    titles = []
+    thumbnails = []
+    descriptions = []
+    dates = []
+    views = []
+
+    for video in recommended_videos:
+        ids.append(User.objects.filter(id=video.id))
+        titles.append(video.title)
+        thumbnails.append(video.thumbnail.url)
+        descriptions.append(video.description)
+        dates.append(video.upload_date)
+        views.append(video.views)
+
+    data = {
+        'ids': ids,
+        'titles': titles,
+        'thumbnails': thumbnails,
+        'descriptions': descriptions,
+        'dates': dates,
+        'views': views
+    }
+
+    return render(request, "home/home.html", {
+        "recommended_videos": recommended_videos,
+        "len": len(recommended_videos)
+    })
 
 def subscriptions(request):
     return render(request, "home/subscriptions-page.html", {})

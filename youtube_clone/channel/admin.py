b1e775a78453611b5c4f89107ff3e550f2508145
TBhutta <89844007+TBhutta@users.noreply.github.com>
2025-04-19 14:57:14 +0100

    Registered models to admin page

--- youtube_clone/channel/admin.py
+++ youtube_clone/channel/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Video, Comment
+from .models import *
 
 admin.site.register(Video)
 admin.site.register(Comment)
+admin.site.register(Playlist)
+admin.site.register(Playlist_Video)
+admin.site.register(Subscriptions)
+admin.site.register(History)
